753fb43d2f0d59b49d5e77c4464bf677bbb67983
KaranSharma0712 <145358668+KaranSharma0712@users.noreply.github.com>
2024-03-13 16:43:48 +0530

    Create LongestCommonSubsequence.cpp

---
+++ LongestCommonSubsequence.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lcs(string s1,string s2){
+    int m=s1.length();
+    int n=s2.length();
+    vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(s1[i-1] == s2[j-1]){
+                dp[i][j] = dp[i-1][j-1]+1;
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+    }
+    for(int i=0;i<=m;i++){
+        for(int j=0;j<=n;j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return dp[m][n];
+}
+
+int main(){
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    int lcsLength = lcs(s1,s2);
+    cout<<"Length of LCS is "<<lcsLength<<endl;
+    
+}
