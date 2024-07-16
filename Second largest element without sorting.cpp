ed4c5b772fadd046844f6ed06534c7a445277c2c
KaranSharma0712 <145358668+KaranSharma0712@users.noreply.github.com>
2024-07-16 21:44:18 +0530

    Update Second largest element without sorting.cpp

--- Second largest element without sorting.cpp
+++ Second largest element without sorting.cpp
@@ -1,3 +1,46 @@
+//optimized solution
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int print2largest(vector<int> &arr) {
+        int n = arr.size();
+        int largest = arr[0];
+        int secondlargest = INT_MIN;
+        if(n==1){
+            return -1;
+        }
+        for(int i = 1; i < n; i++){
+            if (arr[i] > largest ){
+                
+                secondlargest = largest;
+                largest = arr[i];
+                
+            }
+            else{
+                if(arr[i] < largest && secondlargest < arr[i]){
+                    secondlargest = arr[i];
+                }
+            }
+            
+        }
+        if(secondlargest == INT_MIN){
+            return -1;
+        }
+        else if(largest != secondlargest ){
+            return secondlargest;
+        }
+        else{
+            return -1;
+        }
+        
+        
+        
+    }
+};
+
+// other solution
+
 class Solution {
   public:
     // Function returns the second
